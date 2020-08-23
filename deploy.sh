28041cb5cd5750dec6bc5518caf4328a47c0695f
leejaeseung <wasd222@naver.com>
2020-08-23 20:26:17 +0900

    배포 테스트

--- deploy.sh
+++ deploy.sh
@@ -11,14 +11,16 @@ APP_NAME=myServer
 
 CURRENT_PID=$(pgrep -f ${APP_NAME})
 
+echo "> pid : ${CURRENT_PID}"
+
 if [ -z ${CURRENT_PID} ]
 then
     echo "> Not started."
-    npm start
+    sudo npm start
 else
     echo "> kill -9 ${CURRENT_PID}"
     sudo kill -15 ${CURRENT_PID}
     sleep 5
-    npm restart
+    pm2 restart ${APP_NAME}
 fi
 
