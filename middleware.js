1dad49b38c94dd0cc008a365febf00790465964f
leejaeseung <wasd222@naver.com>
2020-08-23 16:30:29 +0900

    배포 테스트

--- middleware.js
+++ middleware.js
@@ -16,7 +16,7 @@ const storage_PF = multer.diskStorage({destination: "uploads/profiles/", filenam
 
 // pug에서 사용될 지역 변수들을 관리해주는 미들웨어
 export const localsMiddleware = (req, res, next) =>{
-    res.locals.siteName = "JTube";
+    res.locals.siteName = "JTubesss";
     res.locals.routes = routes;
 
     res.locals.nowUser = {
