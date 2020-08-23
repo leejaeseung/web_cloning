b1d571590a71cab92ce1a7a20e4d7b477e29b7be
leejaeseung <wasd222@naver.com>
2020-08-23 17:36:00 +0900

    배포 테스트

--- init.js
+++ init.js
@@ -3,4 +3,6 @@ import "./db";
 
 const handleListening = () => console.log("Example app listening on port " + process.env.PORT + "!");
 
+console.log("hi")
+
 app.listen(process.env.PORT, handleListening);
