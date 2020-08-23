7c03ba6fc22b00c214bb0a90ac8e64e9b5411ffa
leejaeseung <wasd222@naver.com>
2020-08-23 19:53:23 +0900

    배포 테스트

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
 REPO=/server
-cd $REPO
+cd ${REPO}
 
 sudo npm install
 
 sudo npm install pm2
+
+APP_NAME=myServer
+
+CURRENT_PID=$(pgrep -f ${APP_NAME})
+
+if [ -z ${CURRENT_PID} ]
+then
+    echo "> Not started."
+else
+    echo "> kill -9 ${CURRENT_PID}"
+    kill -15 ${CURRENT_PID}
+    sleep 5
+fi
 
 sudo npm start
