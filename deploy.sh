5294ffb6365640b33ec1b956c9ffb4c0bcfd8f43
leejaeseung <wasd222@naver.com>
2020-08-24 12:55:14 +0900

    배포 테스트

--- deploy.sh
+++ deploy.sh
@@ -5,20 +5,4 @@ cd ${REPO}
 
 sudo npm install
 
-APP_NAME=myServer
-
-CURRENT_PID=$(pgrep -f ${APP_NAME})
-
-echo "> pid : ${CURRENT_PID}"
-
-if [ -z ${CURRENT_PID} ]
-then
-    echo "> Not started."
-    sudo npm start
-else
-    echo "> kill -9 ${CURRENT_PID}"
-    sudo kill -15 ${CURRENT_PID}
-    sleep 5
-    pm2 restart ${APP_NAME}
-fi
-
+npm run-script dev
